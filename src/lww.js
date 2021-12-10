10f48e156f4bc0318bf08ecbac0e0c2b68268579
kpstar <kpstar.mc@gmail.com>
2021-12-11 04:22:16 +0800

    Review - HK

---
+++ src/lww.js
@@ -0,0 +1,177 @@
+class Lww {
+	constructor() {
+		this._vertexAdded = {};
+		this._vertexRemoved = {};
+		this._edgeAdded = {};
+		this._edgeRemoved = {};
+	}
+
+	// add vertex (key) with timestamp(value)
+	addVertex(vertex) {
+		this._vertexAdded[vertex] = new Date();
+	}
+
+	// vertex in graph
+	// if in vertexAdded and not in vertexRemoved or in vertexRemoved with an earlier timestamp
+	lookupVertex(vertex) {
+		return this._vertexAdded[vertex] &&
+			(!this._vertexRemoved[vertex] || this._vertexAdded[vertex] >= this._vertexRemoved[vertex])
+			? true
+			: false;
+	}
+
+	// if vertex in graph and not used in edge, add in vertexRemoved with timestamp
+	removeVertex(vertex) {
+		if (this.lookupVertex(vertex) && !this.vertexInEdge(vertex)) {
+			this._vertexRemoved[vertex] = new Date();
+		} else {
+			console.log('Cannot remove vertex ', vertex);
+		}
+	}
+
+	// vertex in edge
+	// if edge in graph and vertex in edge
+	vertexInEdge(vertex) {
+		for (let edge in this._edgeAdded) {
+			const vertice = edge.split(',');
+			if (this.lookupEdge(vertice) && (vertice[0] === vertex.toString() || vertice[1] === vertex.toString())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// if both vertices in graph, add edge with timestamp
+	addEdge(vertex1, vertex2) {
+		if (this.lookupVertex(vertex1) && this.lookupVertex(vertex2)) {
+			this._edgeAdded[[vertex1, vertex2]] = new Date();
+		} else {
+			console.log('Cannot add edge ', [vertex1, vertex2]);
+		}
+	}
+
+	// edge in graph
+	// if both vertices in graph
+	// and edge in edgeAdded
+	// and not in edgeRemoved or in edgeRemoved with an earlier timestamp than edgeAdded
+	lookupEdge(edge) {
+		return this.lookupVertex(edge[0]) &&
+			this.lookupVertex(edge[1]) &&
+			this._edgeAdded[edge] &&
+			(!this._edgeRemoved[edge] || this._edgeAdded[edge] >= this._edgeRemoved[edge])
+			? true
+			: false;
+	}
+
+	// if edge in graph, add in edgeRemoved with timestamp
+	removeEdge(edge) {
+		if (this.lookupEdge(edge)) {
+			this._edgeRemoved[edge] = new Date();
+		} else {
+			console.log('Cannot remove edge ', edge);
+		}
+	}
+
+	// get all vertices (in array) connect to vertex
+	connectedVertice(vertex) {
+		let connected = [];
+		for (let edge in this._edgeAdded) {
+			const vertice = edge.split(',');
+			if (this.lookupEdge(vertice)) {
+				if (vertice[0] === vertex.toString()) {
+					connected.push(vertice[1]);
+				} else if (vertice[1] === vertex.toString()) {
+					connected.push(vertice[0]);
+				}
+			}
+		}
+		return connected;
+	}
+
+	findOnePath(source, target) {
+		const path = [];
+
+		if (source === target) {
+			path.push(source);
+			return path;
+		}
+
+		const visited = new Set();
+
+		const connectedVertice = [];
+		this._moveForward(source, path, connectedVertice, visited);
+
+		while (path.length > 0) {
+			const connected = connectedVertice.pop();
+
+			if (connected && connected.length) {
+				const nextVertex = connected.shift();
+				connectedVertice.push(connected);
+
+				if (this.lookupVertex(nextVertex)) {
+					this._moveForward(nextVertex, path, connectedVertice, visited);
+				}
+			} else {
+				connectedVertice.push(connected);
+				this._moveBack(path, connectedVertice, visited);
+				continue;
+			}
+
+			if (path[path.length - 1] === target) {
+				return path;
+			}
+		}
+		return path;
+	}
+
+	_moveForward(vertex, path, connectedVertice, visited) {
+		if (vertex) {
+			path.push(vertex);
+			visited.add(vertex);
+
+			const connected = this.connectedVertice(vertex).filter((v) => !visited.has(v));
+			connectedVertice.push(connected);
+		}
+	}
+
+	_moveBack(path, connectedVertice, visited) {
+		const removedVertex = path.pop();
+
+		if (removedVertex) {
+			visited.delete(removedVertex);
+		}
+		connectedVertice.pop();
+	}
+
+	merge(other) {
+		this.mergeSets(this._vertexAdded, other.getVertexAdded());
+		this.mergeSets(this._vertexRemoved, other.getVertexRemoved());
+		this.mergeSets(this._edgeAdded, other.getEdgeAdded());
+		this.mergeSets(this._edgeRemoved, other.getEdgeRemoved());
+	}
+
+	// merge set2 to set1
+	mergeSets(set1, set2) {
+		for (let element in set2) {
+			set1[element] = set1[element] && set1[element] >= set2[element] ? set1[element] : set2[element];
+		}
+	}
+
+	getVertexAdded() {
+		return Object.assign({}, this._vertexAdded);
+	}
+
+	getVertexRemoved() {
+		return Object.assign({}, this._vertexRemoved);
+	}
+
+	getEdgeAdded() {
+		return Object.assign({}, this._edgeAdded);
+	}
+
+	getEdgeRemoved() {
+		return Object.assign({}, this._edgeRemoved);
+	}
+}
+
+module.exports = Lww;
---
+++ test/lww.test.js
@@ -0,0 +1,204 @@
+const Lww = require('../src/lww');
+
+let lww;
+beforeEach(() => {
+	lww = new Lww();
+});
+it('adds vertex to vertexAdded with current timestamp', () => {
+	const mockDate = mockDateTime(1621930691554);
+	lww.addVertex(1);
+	expect(lww.getVertexAdded()[1]).toEqual(mockDate);
+});
+
+it('lookup vertex is false when vertex not exists', () => {
+	expect(lww.lookupVertex(1)).toBe(false);
+});
+
+it('lookup vertex is true after vertex added', () => {
+	lww.addVertex(1);
+	expect(lww.lookupVertex(1)).toBe(true);
+});
+
+it('cannot remove vertex when vertex not exists', () => {
+	lww.removeVertex(1);
+	expect(lww.getVertexRemoved()[1]).toBe(undefined);
+});
+
+it('removing existing vertex adds it to vertexRemoved with current timestamp', () => {
+	mockDateTime(1621930691554);
+	lww.addVertex(1);
+	const mockDate = mockDateTime(1621930691555);
+	lww.removeVertex(1);
+	expect(lww.getVertexRemoved()[1]).toEqual(mockDate);
+});
+
+it('lookup vertex is false after vertex removed', () => {
+	mockDateTime(1621930691554);
+	lww.addVertex(1);
+	mockDateTime(1621930691555);
+	lww.removeVertex(1);
+	expect(lww.lookupVertex(1)).toBe(false);
+});
+
+it('cannot add edge if vertices not exists', () => {
+	lww.addEdge(1, 2);
+	expect(lww.getEdgeAdded()[[1, 2]]).toBe(undefined);
+});
+
+it('adds edge to edgeAdded with current timestamp', () => {
+	const mockDate = mockDateTime(1621930691554);
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addEdge(1, 2);
+	expect(lww.getEdgeAdded()[[1, 2]]).toEqual(mockDate);
+});
+
+it('lookup edge is false when edge not exists', () => {
+	expect(lww.lookupEdge([1, 2])).toBe(false);
+});
+
+it('lookup edge is true after edge added', () => {
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addEdge(1, 2);
+	expect(lww.lookupEdge([1, 2])).toBe(true);
+});
+
+it('cannot remove edge when edge not exists', () => {
+	lww.removeEdge([1, 2]);
+	expect(lww.getEdgeRemoved()[[1, 2]]).toBe(undefined);
+});
+
+it('removing existing edge adds it to edgeRemoved with a timestamp', () => {
+	mockDateTime(1621930691554);
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addEdge(1, 2);
+	const mockDate = mockDateTime(1621930691555);
+	lww.removeEdge([1, 2]);
+	expect(lww.getEdgeRemoved()[[1, 2]]).toEqual(mockDate);
+});
+
+it('lookup edge is false after edge removed', () => {
+	mockDateTime(1621930691554);
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addEdge(1, 2);
+	mockDateTime(1621930691555);
+	lww.removeEdge([1, 2]);
+	expect(lww.lookupEdge([1, 2])).toBe(false);
+});
+
+it('cannot remove vertex if it is used in edge', () => {
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addEdge(1, 2);
+	lww.removeVertex(1);
+	expect(lww.getVertexRemoved()[1]).toBe(undefined);
+});
+
+it('get connected vertices for a vertice', () => {
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addVertex(3);
+	lww.addEdge(1, 2);
+	lww.addEdge(2, 3);
+	expect(lww.connectedVertice(1)).toEqual(['2']);
+	expect(lww.connectedVertice(2)).toEqual(['1', '3']);
+});
+
+it('vertex not in edge', () => {
+	lww.addVertex(1);
+	expect(lww.vertexInEdge(1)).toEqual(false);
+});
+
+it('vertex in edge', () => {
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addEdge(1, 2);
+	expect(lww.vertexInEdge(1)).toEqual(true);
+	expect(lww.vertexInEdge(2)).toEqual(true);
+});
+
+it('empty path if vertices not connected', () => {
+	expect(lww.findOnePath('1', '2')).toEqual([]);
+});
+
+it('find path for vertice connect to itself', () => {
+	lww.addVertex(1);
+	expect(lww.findOnePath('1', '1')).toEqual(['1']);
+});
+
+it('find any path between two vertices', () => {
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addVertex(3);
+	lww.addVertex(4);
+	lww.addEdge(1, 2);
+	lww.addEdge(1, 4);
+	lww.addEdge(4, 3);
+	console.log(lww.findOnePath('1', '3'));
+	expect(lww.findOnePath('1', '3')).toEqual(['1', '4', '3']);
+});
+
+it('merge payload', () => {
+	const set1 = { 1: '2021-05-25T08:18:11.554Z', 2: '2021-05-25T08:18:11.554Z' };
+	const set2 = { 2: '2021-05-25T08:18:11.556Z', 3: '2021-05-25T08:18:11.556Z' };
+	const mergedSet = {
+		1: '2021-05-25T08:18:11.554Z',
+		2: '2021-05-25T08:18:11.556Z',
+		3: '2021-05-25T08:18:11.556Z',
+	};
+	lww.mergeSets(set1, set2);
+	expect(set1).toEqual(mergedSet);
+});
+
+it('merge lww with other', () => {
+	// lww
+	const mockDate1 = mockDateTime(1621930691554);
+	lww.addVertex(1);
+	lww.addVertex(2);
+	lww.addVertex(3);
+	lww.addEdge(1, 2);
+	lww.addEdge(2, 3);
+	const mockDate2 = mockDateTime(1621930691555);
+	lww.removeEdge([2, 3]);
+	const mockDate3 = mockDateTime(1621930691556);
+	lww.removeVertex(3);
+	// other
+	const other = new Lww();
+	const mockDate4 = mockDateTime(1621930691554);
+	other.addVertex(4);
+	other.addVertex(5);
+	other.addEdge(4, 5);
+	// merge
+	lww.merge(other);
+	// vertexAdded after merged
+	expect(lww.getVertexAdded()).toEqual({
+		1: mockDate1,
+		2: mockDate1,
+		3: mockDate1,
+		4: mockDate4,
+		5: mockDate4,
+	});
+	// vertexRemoved after merged
+	expect(lww.getVertexRemoved()).toEqual({ 3: mockDate3 });
+	// edgeAdded after merged
+	expect(lww.getEdgeAdded()).toEqual({
+		'1,2': mockDate1,
+		'2,3': mockDate1,
+		'4,5': mockDate4,
+	});
+	// edgeRemoved after merged
+	expect(lww.getEdgeRemoved()).toEqual({ '2,3': mockDate2 });
+});
+
+const mockDateTime = (mockDT) => {
+	jest.useFakeTimers('modern');
+	jest.setSystemTime(new Date(mockDT).getTime());
+	return new Date();
+};
+
+afterAll(() => {
+	jest.useRealTimers();
+});
